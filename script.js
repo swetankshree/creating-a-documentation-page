80143019e5c2f5c3fde2f9c3fb319b089819dce3
swetankshree <110844738+swetankshree@users.noreply.github.com>
2022-09-18 17:02:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let fontChangeElement=document.getElementById("body");
+let font2=document.getElementById("font_2");
+font2.addEventListener("click", function(){
+    fontChangeElement.style.fontSize="x-large";
+});
+
+//let fontChangeElement=document.getElementById("body");
+let font1=document.getElementById("font_1");
+font1.addEventListener("click", function(){
+    fontChangeElement.style.fontSize="medium";
+});
+
+let font05=document.getElementById("font_0.5");
+font05.addEventListener("click", function(){
+    fontChangeElement.style.fontSize="small";
+});
